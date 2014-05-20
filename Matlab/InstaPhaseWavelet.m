6c15c38074b9acebbf01ecc227c5f669d50e91cc
Alexandre Laurin <laurin@sfu.ca>
2014-05-20 16:50:41 -0700

    deleted some old useless matlab files

--- Matlab/InstaPhaseWavelet.m
+++
@@ -1,8 +0,0 @@
-function [ InstaPhase ] = InstaPhaseWavelet( Signal1, dt )
-%[ InstaPhase ] = InstaPhaseWavelet( Signal1 )
-%%
-Time = (0:dt:length(Signal1)-1);
-[Wave,Period,~,~] = wavelet(Signal1,dt,0, 0.25, 100, 25);
-figure(3); clf; imagesc(Time,Period,2.*abs(Wave));
-end
-
