b327274dcccdb44accbe3c5b5393e4fbf70c1e52
Alexandre Laurin <laurin@sfu.ca>
2014-05-21 11:03:37 -0700

    Normalized the wavelet_interp function for inputs and outputs

--- Matlab/wavelet_interp.m
+++ Matlab/wavelet_interp.m
@@ -1,27 +1,37 @@
-function [freq,power,WN,scales,phase_max_u,phase_max_w,wi] =  wavelet_interp(x)
-
-res=256; %frequency resolution
-dt=1; %sampling rate
-dj=0.0625; %wavelet interval
+function [TDPhase] =  wavelet_interp(TDSignal, DT, Graphic)
+%%
+Time = 0:length(TDSignal)-1;
+Time=Time.*DT;
+dj=0.04; %wavelet interval
 J1=127; %total # of scales
 pad=1; %zero-pad signals
-s0=2*dt; %set initial scale
+s0=0.5; %set initial scale
 mother='Morlet'; %wavelet shape
 param=6; %initial wavelet order
 
 %computes the wavelets
-[WN,~,scales] = wavelet(x,dt,pad,dj,s0,J1,mother,param);
-freq=1./(1.033*scales);
-power = abs(WN).^2;
-
-siz = size(power,2);
-
-for k = 1:siz
-    [~,idx(k)] = max(power(:,k));
-    phase_max(k) = angle(WN(idx(k),k));
+[WN,Period,~] = wavelet(TDSignal,DT,pad,dj,s0,J1,mother,param);
+Power = abs(WN).^2;
+%Freqs=1./(1.033.*Scale);
+if Graphic
+    figure(1);clf;
+    mesh(Time, Period, Power) 
+    view(45,90);
+    colormap(hot)
 end
-phase_max_u = unwrap(phase_max)';
-phase_max_w = wrapToPi(phase_max_u)';
-
-wi = (diff(phase_max_u).*2*2/pi)';
+%%
+Size = size(Power,2);
+Phase_max = zeros(Size,1);
+for k = 1:Size
+    [~,TempPeriod] = max(Power(:,k));
+    Phase_max(k) = angle(WN(TempPeriod,k));
+end
+Phase_max_u = unwrap(Phase_max)';
+size(Phase_max)
+TDPhase = wrapToPi(Phase_max_u)';
+if Graphic
+   figure(2); clf
+   plot(TDPhase);
+end
+%wi = (diff(Phase_max_u).*2*2/pi)';
 end
