5f6f9026e8f6273f4845e98387cdfbc909401553
Nick Mitrou <nmitrou@sfu.ca>
2014-05-22 15:54:14 -0700

    TV signal

--- Matlab/Generate_TV_signal.m
+++ Matlab/Generate_TV_signal.m
@@ -1,18 +1,18 @@
 %%% generate TV signal %%%
 
-f1 = 0.04; %low frequency
-f2 = 0.1; % high frequency
+f1 = 0.1; %low frequency
+f2 = 0.3; % high frequency
 len = 500; % duration in seconds
 AM = 1; % amplitude modulation 
-FM = 1; % frequency modulation
-noi = 1; % add GWN
+FM = 0; % frequency modulation
+noi = 0; % add GWN
 t = 1:len;
 
 [x]=test_sig_AMFM(f1,f2,len,AM,FM,noi);
 
 
  
-res=256; %frequency resolution
+% res=256; %frequency resolution
 dt=1; %sampling rate
 dj=0.0625; %wavelet interval
 J1=127; %total # of scales
@@ -20,8 +20,10 @@ pad=1; %zero-pad signals
 s0=2*dt; %set initial scale
 mother='Morlet'; %wavelet shape
 param=6; %initial wavelet order
-az = 63;
-el = 46;
+% az = 63;
+% el = 46;
+az = 45;
+el = 90;
 
 %computes the wavelets
 [WN1,~,scales] = wavelet(x,dt,pad,dj,s0,J1,mother,param);
@@ -30,6 +32,7 @@ power = abs(WN1).^2;
 
 mesh(t,freq,power)
 view(az,el)
+colormap(hot)
 xlabel('Time (s)')
 ylabel('Frequency (Hz)')
 zlabel('Power (Units^2/Hz)')
