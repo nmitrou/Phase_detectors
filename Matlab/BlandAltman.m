52027468adb02104d4e0011cd70be13f8b3be53e
Nick Mitrou <nmitrou@sfu.ca>
2014-06-05 14:26:58 -0700

    run Bland-Altmanon two sets of data

---
+++ Matlab/BlandAltman.m
@@ -0,0 +1,18 @@
+function [Difference,MeanEst,MeanDiff,STDDiff] = BlandAltman(Sig1,Sig2,Graphic)
+
+
+EstPhases = [Sig1; Sig2]';
+
+MeanEst = mean(EstPhases,2);
+
+Difference = Sig1 - Sig2;
+
+MeanDiff = mean(Difference);
+STDDiff = std(Difference);
+
+if Graphic == 1
+    plot(MeanEst,Difference,'ko')
+    xlabel 'Phase'
+    ylabel 'Difference'
+end
+end
