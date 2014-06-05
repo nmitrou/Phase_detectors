fc30b8a4088b1fb5aad9b28a3f53bce659bbcdc1
Nick Mitrou <nmitrou@sfu.ca>
2014-06-05 14:29:02 -0700

    added bandpass filter

--- Matlab/hilbert_interp.m
+++ Matlab/hilbert_interp.m
@@ -1,4 +1,4 @@
-function [phh,phhu,phhw,w,wi] =  hilbert_interp(Signal,Dt,f1,f2,Graphic)
+function [TDPhase] =  hilbert_interp(Signal,Dt,f1,f2,Graphic)
 %% Hilbert Interp %%
 % function purpose is to use the hilbert transform of a signal and estimate
 % the instantaneous phase and frequency of the signal
@@ -9,11 +9,7 @@ function [phh,phhu,phhw,w,wi] =  hilbert_interp(Signal,Dt,f1,f2,Graphic)
 % f2 - upper bound for the frequency range of the oscillation of interest
 % Graphic - 1 for plots, 0 for no plots
 %%%%%%%%%% Outputs %%%%%%%%%%
-% phh - Instantaneous phase wrapped to 2pi
-% phhu - Instantaneous phase unwrapped
-% phhw - Instantaneous unwrapped phase rewrapped to Pi
-% w = Instantaneous frequency
-% wi = Instantaneous frequency with phase slipes interpolated
+% TDPhase - unwrapped instantaneous phase with phase slips interpolated
 
 Fs = 1/Dt;  % Sampling Frequency
 %FOR MYO
@@ -27,9 +23,9 @@ Signal_f = filtfilt(b,a,Signal); %bandpass filter the input signal
 
 xh = hilbert(Signal_f);
 phh = angle(xh);
-phhu = unwrap(phh);
+TDPhase = unwrap(phh);
 
-    w = diff(phhu).*(2*2/pi);
+    w = diff(TDPhase).*(2*2/pi);
     wi = w;
 %% find data points where inst. frequency is outside filter pass band, and interpolate across those points
 t = Dt:Dt:length(Signal_f)*Dt;
